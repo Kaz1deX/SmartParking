2ce8b383c788a4746277052fbdc49036908b13d2
Kaz1deX <max2003255@gmail.com>
2024-04-11 17:09:08 +0300

    Changed UI Settings screen

--- app/src/main/java/com/example/smartparking/ui/screen/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/smartparking/ui/screen/settings/SettingsScreen.kt
@@ -1,7 +1,11 @@
 package com.example.smartparking.ui.screen.settings
 
 import android.content.Context
+import android.provider.Settings.Global.getString
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,8 +14,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -19,8 +25,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -47,110 +56,49 @@ fun SettingsScreen(navController: NavHostController, context: Context) {
         Spacer(modifier = Modifier
             .padding(top = 50.dp)
         )
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 30.dp, end = 30.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = "Поддержка",
-                color = Color.Black,
-                fontSize = 15.sp,
-                textAlign = TextAlign.Center
-            )
-            IconButton(
-                modifier = Modifier
-                    .height(35.dp)
-                    .width(35.dp),
-                onClick = { /*TODO*/ }
-            ) {
-                Icon(painter = painterResource(id = R.drawable.buttonarrow), contentDescription = "")
+
+        val settingsName = stringArrayResource(id = R.array.settings)
+
+        for(i in settingsName.indices) {
+            SettingsItem(settingsName[i])
+            if(i != (settingsName.size - 1)) {
+                Divider(modifier = Modifier
+                    .padding(start = 30.dp, end = 30.dp, top = 15.dp, bottom = 15.dp),
+                    thickness = 1.dp,
+                    color = DividerGrey
+                )
             }
         }
+    }
+}
 
-        Divider(modifier = Modifier
-            .padding(start = 30.dp, end = 30.dp, top = 15.dp, bottom = 15.dp),
-            thickness = 1.dp,
-            color = DividerGrey
-        )
 
+@Composable
+fun SettingsItem(name: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 15.dp, end = 15.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .height(50.dp)
+            .clickable {
+
+            }
+    ) {
         Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 30.dp, end = 30.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
+                .fillMaxSize()
+                .padding(start = 15.dp, end = 15.dp),
         ) {
             Text(
-                text = "Уведомления",
+                text = name,
                 color = Color.Black,
                 fontSize = 15.sp,
                 textAlign = TextAlign.Center
             )
-            IconButton(
-                modifier = Modifier
-                    .height(35.dp)
-                    .width(35.dp),
-                onClick = { /*TODO*/ }
-            ) {
-                Icon(painter = painterResource(id = R.drawable.buttonarrow), contentDescription = "")
-            }
-        }
-
-        Divider(modifier = Modifier
-            .padding(start = 30.dp, end = 30.dp, top = 15.dp, bottom = 15.dp),
-            thickness = 1.dp,
-            color = DividerGrey
-        )
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 30.dp, end = 30.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = "Информация",
-                color = Color.Black,
-                fontSize = 15.sp,
-                textAlign = TextAlign.Center
-            )
-            IconButton(
-                modifier = Modifier
-                    .height(35.dp)
-                    .width(35.dp),
-                onClick = { /*TODO*/ }
-            ) {
-                Icon(painter = painterResource(id = R.drawable.buttonarrow), contentDescription = "")
-            }
-        }
-
-        Divider(modifier = Modifier
-            .padding(start = 30.dp, end = 30.dp, top = 15.dp, bottom = 15.dp),
-            thickness = 1.dp,
-            color = DividerGrey
-        )
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 30.dp, end = 30.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = "О приложении",
-                color = Color.Black,
-                fontSize = 15.sp,
-                textAlign = TextAlign.Center
-            )
-            IconButton(
-                modifier = Modifier
-                    .height(35.dp)
-                    .width(35.dp),
-                onClick = { /*TODO*/ }
-            ) {
-                Icon(painter = painterResource(id = R.drawable.buttonarrow), contentDescription = "")
-            }
+            Icon(painter = painterResource(id = R.drawable.buttonarrow), contentDescription = "")
         }
     }
 }
