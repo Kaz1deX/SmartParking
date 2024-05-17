d3896d1c8331a2f14004fc6839639fd6be48760b
Kaz1deX <max2003255@gmail.com>
2024-05-17 17:42:43 +0300

    Fix Gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,13 +74,14 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     // Ktor
-    implementation("io.ktor:ktor-client-core:2.3.10")
-    implementation("io.ktor:ktor-client-cio:2.3.10")
+    implementation("io.ktor:ktor-client-core:2.3.5")
+    implementation("io.ktor:ktor-client-cio:2.3.5")
     implementation("io.ktor:ktor-client-android:2.3.5")
     implementation("io.ktor:ktor-client-serialization:2.3.5")
     implementation("io.ktor:ktor-client-logging:2.3.5")
     implementation("io.ktor:ktor-client-content-negotiation:2.3.5")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.10")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.5")
     implementation("io.ktor:ktor-client-logging:2.3.5")
-    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("ch.qos.logback:logback-classic:1.2.6")
+
 }
