754099760409b89ed94068e508b9608d5eb6100b
Kaz1deX <max2003255@gmail.com>
2024-05-19 16:04:26 +0300

    Changed repository, create function for check internet connection

--- app/src/main/java/com/example/smartparking/data/repositories/MainRepository.kt
+++ app/src/main/java/com/example/smartparking/data/repositories/MainRepository.kt
@@ -1,35 +1,76 @@
 package com.example.smartparking.data.repositories
 
+import android.content.Context
 import com.example.smartparking.data.api.ApiService
 import com.example.smartparking.data.model.Car
 import com.example.smartparking.data.model.CarReceive
+import com.example.smartparking.data.model.Parking
 import com.example.smartparking.data.model.UserLogin
 import com.example.smartparking.data.model.UserRegister
+import com.example.smartparking.data.model.toCar
+import com.example.smartparking.data.model.toCarEntity
+import com.example.smartparking.data.room.dao.CarDao
+import com.example.smartparking.util.isOnline
 
-class MainRepository {
+class MainRepository(
+    private val carDao: CarDao,
+    private val context: Context
+) {
     private val apiService by lazy {
         ApiService.create()
     }
 
     suspend fun auth(login: String, password: String): String {
-        val userLogin = UserLogin(login, password)
-        val authResponse = apiService.auth(userLogin)
-        return authResponse.token
+        if (isOnline(context)) {
+            val userLogin = UserLogin(login, password)
+            val authResponse = apiService.auth(userLogin)
+            return authResponse.token
+        } else {
+            return "No internet connection"
+        }
     }
 
     suspend fun register(login: String, password: String, username: String, email: String): String {
-        val userRegister = UserRegister(login, email, password, username)
-        val registerResponse = apiService.register(userRegister)
-        return registerResponse.token
+        if (isOnline(context)) {
+            val userRegister = UserRegister(login, email, password, username)
+            val registerResponse = apiService.register(userRegister)
+            return registerResponse.token
+        } else {
+            return "No internet connection"
+        }
     }
 
     suspend fun getCars(login: String): List<Car> {
-        val cars = apiService.getCars(login)
-        return cars
+        if (isOnline(context)) {
+            val cars = apiService.getCars(login)
+            if (cars.isNotEmpty()) {
+                cars.forEach {
+                    val carEntity = it.toCarEntity()
+                    carDao.insertCar(carEntity)
+                }
+            }
+            return cars
+        } else {
+            val cars = carDao.getAllCars().map { it.toCar() }
+            return cars
+        }
     }
 
     suspend fun addCar(car: CarReceive): Boolean {
         val addCar = apiService.addCar(car)
+        if (addCar) {
+            val carEntity = car.toCarEntity()
+            carDao.insertCar(carEntity)
+        }
         return addCar
+    }
+
+    suspend fun getParking(): List<Parking> {
+        if (isOnline(context)) {
+            val parking = apiService.getParking()
+            return parking
+        } else {
+            return emptyList()
+        }
     }
 }
