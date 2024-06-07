4ab5df546bf68d1b721fbfcf6d2d1145c18c2072
Kaz1deX <max2003255@gmail.com>
2024-06-07 17:10:54 +0300

    Fixed

--- app/src/main/java/com/example/smartparking/navigation/Screen.kt
+++ app/src/main/java/com/example/smartparking/navigation/Screen.kt
@@ -5,7 +5,7 @@ import com.example.smartparking.util.Constants.KEY_PARKING_ID
 
 sealed class Screen(val route: String, var icon: Int = R.drawable.ic_launcher_foreground, var title: String) {
     data object ProfileScreen: Screen(route = "profile_screen", icon = R.drawable.profile_icon, title = "Профиль")
-    data object MapScreen: Screen(route = "map_screen", icon = R.drawable.map_icon, title = "Карта")
+    data object MapScreen: Screen(route = "map_screen", icon = R.drawable.map_icon, title = "Парковки")
     data object FavouritesScreen: Screen(route = "favourites_screen", icon = R.drawable.favourites_icon, title = "Избранное")
     data object SettingsScreen: Screen(route = "settings_screen", icon = R.drawable.settings_icon, title = "Настройки")
     data object LoginScreen: Screen(route = "login_screen", title = "LoginScreen")
